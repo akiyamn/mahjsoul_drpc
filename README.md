378a9eae4380554e6733c176d470fd9e64ad65ca
akiyamn <10993186+akiyamn@users.noreply.github.com>
2023-04-26 22:21:02 +1000

    Initial commit 🀄

---
+++ README.md
@@ -0,0 +1,5 @@
+# MahjongSoul Discord Rich Presence Script
+
+This is a Python script that reads Firefox data to display a Rich Presence status if you have MahjongSoul open in a tab.
+
+This script uses Python 3.10 and was designed for use on Linux. Untested on Windows and Mac.
---
+++ mahjsoul.py
@@ -0,0 +1,91 @@
+import sys
+import pathlib
+import lz4.block
+import json
+from pypresence import Presence
+import time
+
+URL_MATCH = "https://mahjongsoul.game.yo-star.com"
+CLIENT_ID = "1100743919324635218"
+RPC_IMAGE = "ichihime-rage"
+SLEEP_SECONDS = 30
+
+class Tab:
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+
+    def __str__(self):
+        return f"{self.title} - {self.url}"
+
+    def __repr__(self):
+        return str(self)
+
+
+def list_firefox_files():
+    path = pathlib.Path.home().joinpath('.mozilla/firefox')
+    return path.glob('*default*/sessionstore-backups/recovery.js*')
+
+
+def decompress_firefox_json(path):
+    data = path.read_bytes()
+    raw = lz4.block.decompress(data[8:])  # Remove mozLz40\0 header
+    return json.loads(raw)
+
+
+def extract_tabs_from_json(json):
+    buffer = []
+    for window in json['windows']:
+        for tab in window['tabs']:
+            index = tab['index'] - 1
+            url = tab['entries'][index]['url'].removesuffix("/")
+            title = tab['entries'][index]['title']
+            tab_struct = Tab(title, url)
+            buffer.append(tab_struct)
+    return buffer
+
+
+def check_match(tab):
+    return URL_MATCH == tab.url
+
+
+def is_matching_tab_open():
+    files = list(list_firefox_files())
+    for f in files:
+        json = decompress_firefox_json(f)
+        matches = map(check_match, extract_tabs_from_json(json))
+        if any(matches):
+            return True
+
+    return False
+
+
+def set_rich_presence(rpc, start_time):
+    rpc.update(large_image=RPC_IMAGE,
+               details="Playing in browser", start=start_time)
+
+
+def main():
+    print("MahjongSoul RPC script init")
+    rpc = Presence(CLIENT_ID)
+    rpc.connect()
+    print("Discord RPC connected")
+    start_time = None
+    while True:
+        try:
+            if is_matching_tab_open():
+                start_time = int(
+                    time.time()) if start_time is None else start_time
+                set_rich_presence(rpc, start_time)
+            else:
+                rpc.clear()
+                start_time = None
+            time.sleep(SLEEP_SECONDS)
+        except KeyboardInterrupt:
+            break
+    print("Closing RPC client...")
+    rpc.close()
+    print("Finished")
+
+
+main()
